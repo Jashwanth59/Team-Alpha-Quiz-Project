7ebf9f782a191817de0e42c72b0b058e3c85cc08
GhostGekko <m.gipson717@gmail.com>
2025-10-25 17:42:30 -0700

    More modifications to pages, added some js for a basic timer

---
+++ my_script.js
@@ -0,0 +1,32 @@
+function timer() {
+    // Set the date we're counting down to
+    var time = new Date();
+    const countDownDate = new Date(time); // Create a new Date object to avoid modifying 'now'
+    countDownDate.setHours(countDownDate.getHours() + 1); // Add one hour
+
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+        // Get today's date and time
+        var now = new Date().getTime();
+            
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+            
+        // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+        // Output the result in an element with id="demo"
+        document.getElementById("timer").innerHTML = hours + "h "
+        + minutes + "m " + seconds + "s ";
+            
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
+
